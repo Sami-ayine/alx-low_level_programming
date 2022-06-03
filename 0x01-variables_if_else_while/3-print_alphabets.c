6d4b8440ca0c9b27c91b3b37609ea3590c7231e8
Sami-ayine <samiayine3@gmail.com>
2022-06-03 04:33:36 -0700

    another alphabet order

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,17 @@
-
-  
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 char alp;
-for ( alp = 'a'; alp <= 'z'; alp++)
+for (alp = 'a'; alp <= 'z'; alp++)
 putchar(alp);
-for (alp ='A'; alp <= 'Z'; alp++)
+for (alp = 'A'; alp <= 'Z'; alp++)
 putchar(alp);
 putchar('\n');
 return (0);
