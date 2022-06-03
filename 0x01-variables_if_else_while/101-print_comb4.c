5010ae6444ceee571cdb5e5d6eb1cfcc85e30595
Sami-ayine <samiayine3@gmail.com>
2022-06-03 07:07:38 -0700

    damn 4th combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,7 @@
 */
 int main(void)
 {
-
 int d, p, q;
-
 for (d = '0'; d < '9'; d++)
 {
 for (p = d + 1; p <= '9'; p++)
@@ -21,18 +19,14 @@ for (q = p + 1; q <= '9'; q++)
 if ((p != d) != q)
 {
 putchar(d);
-putchar(p);
-putchar(q);														
+putchar(p);									putchar(q);
 if (d == '7' && p == '8')
 continue;
-putchar(',')
-putchar(' ');
+putchar(',');									putchar(' ');
 }
 }
 }
 }
 putchar('\n');
-
 return (0);
 }
-
