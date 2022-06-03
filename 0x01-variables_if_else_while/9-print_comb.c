37e6b2a88612737aa9b4b79c0a2d2e204ef85b9e
Sami-ayine <samiayine3@gmail.com>
2022-06-03 05:43:39 -0700

    combination of numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,16 +11,12 @@
 int main(void)
 {
 int d;
-
 for (d = '0'; d <= '9'; d++)
 {
-putchar(d);
-										if (d != '9')
-										{
-										putchar(',');
-																				putchar(' ');
-										}										}
+putchar(d);									if (d != '9')
+{
+putchar(',');
+putchar(' ');									}										}
 putchar('\n');
-
 return (0);
 }
