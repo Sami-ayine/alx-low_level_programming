cad596403e1eef67340b287232302bb7712e9b45
Sami-ayine <samiayine3@gmail.com>
2022-06-03 04:44:48 -0700

    exception of e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char low, e, q;
+e = 'e';
+q = 'q';
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+if ((alp != e && alp != q)
+putchar(low);
+}
+putchar('\n');
+return (0);
+}
