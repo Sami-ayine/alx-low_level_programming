57906f9f95aba7f3112e86edf6291872cb05588b
Sami-ayine <samiayine3@gmail.com>
2022-06-07 10:31:55 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,13 @@
 */
 int main(void)
 {
-char str[] = "_putchar";
-int x;
-int strLen = sizeof(str);
-for (x = 0; x < strLen; x++)
-{
-_putchar(str[x]);
-}
-_putchar('\n');
-return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');                                                                                                                                                  
+_putchar('t');                                                                                                                                                  
+_putchar('c');                                                                                                                                                  
+_putchar('h');                                                                                                                                                  
+_putchar('a');                                                                                                                                                  
+_putchar('r');                                                                                                                                                  
+_putchar('\n');                                                                                                                                                 return (0);
 }
