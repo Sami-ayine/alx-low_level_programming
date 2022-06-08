106ed7714f74b5fceb5d3a6ac5e53146caa90501
Sami-ayine <samiayine3@gmail.com>
2022-06-08 04:14:03 -0700

    damn putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
+
 /**
-* main - Check description
-* Description: It prints the word _putchar, followed by a new line.
-* Return: 0.
+* main - Entry point
+*
+* Return: Alway 0 (Success)
 */
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
