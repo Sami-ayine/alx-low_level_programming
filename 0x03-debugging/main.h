3c5b65726f195950df218018590853798f2a9c98
Sami-ayine <samiayine3@gmail.com>
2022-06-09 12:53:01 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,8 @@
-int main(void)                                                              int main(void)
-int main(void)                                                              void print_remaining_days(int month, int day, int year)
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
