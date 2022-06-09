befc0bcb5adb1e659f78de26c38f10b02fc273e0
Sami-ayine <samiayine3@gmail.com>
2022-06-09 06:51:00 -0700

    tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -40,7 +40,8 @@ else if (k >= 10 && k < 100)
 _putchar(',');
 _putchar(' ');                                                              
 _putchar(' ');                                                     
-_putchar((k / 10) + '0');                                                   _putchar((k % 10) + '0');                                                   
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
 }
 else if (k >= 100)
 {
