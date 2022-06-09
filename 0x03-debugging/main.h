9c7b570289382fac3271f4742210e4a35a2fd8c5
Sami-ayine <samiayine3@gmail.com>
2022-06-09 09:24:10 -0700

    adding main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,4 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+positive_or_negative()
+-Wall -Werror -Wextra -pedantic
+2-largest_number.c
+print_remaining_days()
