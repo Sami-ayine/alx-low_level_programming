b5fa333e5a981311a1d70ccefa0a9bbc0e117991
Sami-ayine <samiayine3@gmail.com>
2022-06-09 06:05:32 -0700

    upto 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,35 +4,29 @@
 * print_to_98 -prints all natural numbers from n to 98
 * @n :print from this number
 */
+
 void print_to_98(int n)
 {
-if (n < 98)
+if (n <= 98)
 {
 while (n <= 98)
 {
-printf("%d", n);
-if (n != 98)
-{
-printf(",");
-}
-n++;
+if (n ==98)
+printf("%d\n", n);
+else
+printf("%d, ", n);
+n = n + 1;
 }
 }
 else if (n > 98)
 {
 while (n >= 98)
 {
-printf("%d", n);
-if (n != 98)
-{
-printf(",");
-}
-n--;
-}
-}
+if (n == 98)
+printf("%d\n", n);
 else
-{
-printf("98");
+printf("%d, ", n);
+n = n - 1;
 }
-printf("\n");
+}
 }
