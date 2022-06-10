43ebe187372d11ffb7e23298377d294460d8f2fb
Sami-ayine <samiayine3@gmail.com>
2022-06-10 02:32:29 -0700

    to nine from one

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * print_numbers - a function that prints the numbers, from 0 to 9,
@@ -8,11 +7,8 @@
 */
 void print_numbers(void)
 {
-int i;
-
-for (i = 0; i < 10; i++)
-{
-_putchar(i + '0');
-}
-_putchar('\n');
+	int i = '0';
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
