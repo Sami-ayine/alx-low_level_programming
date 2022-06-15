e69014849b61c5a1dec9527d845faab5ca542b14
Sami-ayine <samiayine3@gmail.com>
2022-06-15 13:53:52 -0700

    4th task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse
+ * @a : pointer to int params
+ * @n : pointer to int params
+ * Return: no
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, j = n - 1, tmp;
+while (i < j)
+{
+tmp = a[i];
+a[i] = a[i];
+a[j] = tmp;
+i++;
+j--;
+}
+}
