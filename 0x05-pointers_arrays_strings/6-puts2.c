746a93c77fdb40cbbaa5f09ad8f10641afa2d99f
Sami-ayine <samiayine3@gmail.com>
2022-06-17 03:30:41 -0700

    print out every other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,17 +8,14 @@
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int len = 0, i = 0;
 
-	while (str[i] != '\0')
-	{
-		i++;
-	}
+	while (str[len] != '\0')
+		len++;
+	
+	len -= 1;
 
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
-	}
+	for (; i <= len; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
